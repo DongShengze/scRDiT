e1b7907a1412c8027c67e250ea7180c735ce3c46
DongShengze <15522138985@163.com>
2024-01-30 15:16:06 +0800

    update

--- settings.py
+++ settings.py
@@ -6,12 +6,12 @@ parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
 # choose dataset
-args.dataset_path = 'datasets/fibroblast_datas.npy'
-# args.dataset_path = 'datasets/malignant_datas.npy'
+# args.dataset_path = 'datasets/fibroblast_datas.npy'
+args.dataset_path = 'datasets/malignant_datas.npy'
 
 # training settings
-args.run_name = 'fibroblast'  # This will determine the savepath of checkpoints!
-# args.run_name = 'malignant'
+# args.run_name = 'fibroblast'  # This will determine the savepath of checkpoints!
+args.run_name = 'malignant'
 
 args.epochs = 1600  # epochs of training
 args.batch_size = 16  # depends on your GPU memory size
@@ -19,5 +19,5 @@ args.gene_size = 2000  # size of gene set
 args.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 args.lr = 3e-4  # learning rate
 args.save_frequency = 20  # how many epochs to save a checkpoint
-args.ckpt = True  # load checkpoint or not
+args.ckpt = False  # load checkpoint or not
 args.ckpt_epoch = 0  # which checkpoint to load
